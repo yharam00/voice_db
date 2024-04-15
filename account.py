196f1d8aebd0862f390196286e7724e7ad888d6a
yharam00 <yharam00@gmail.com>
2024-04-16 07:26:14 +0900

    baseline ver.

---
+++ account.py
@@ -0,0 +1,179 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+from firebase_admin import auth
+import json
+import requests
+
+my_firebase_api_key = "AIzaSyCLEIngjMhSbU8_WOBOqS5b12UldFXw4dw"
+
+cred = credentials.Certificate("first-1b7e8-cd6fccd0e9d3.json")
+firebase_admin.initialize_app(cred, {'storageBucket': 'first-1b7e8.appspot.com'})
+def app():
+# Usernm = []
+    st.title('Welcome to :violet[Voice DB] :male-technologist:')
+
+    if 'username' not in st.session_state:
+        st.session_state.username = ''
+    if 'useremail' not in st.session_state:
+        st.session_state.useremail = ''
+
+
+    def sign_up_with_email_and_password(email, password, username=None, return_secure_token=True):
+        try:
+            rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp"
+            payload = {
+                "email": email,
+                "password": password,
+                "returnSecureToken": return_secure_token
+            }
+            if username:
+                payload["displayName"] = username 
+            payload = json.dumps(payload)
+            r = requests.post(rest_api_url, params={"key": my_firebase_api_key}, data=payload)
+            try:
+                return r.json()['email']
+            except:
+                st.warning(r.json())
+        except Exception as e:
+            st.warning(f'Signup failed: {e}')
+
+    def sign_in_with_email_and_password(email=None, password=None, return_secure_token=True):
+        rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+
+        try:
+            payload = {
+                "returnSecureToken": return_secure_token
+            }
+            if email:
+                payload["email"] = email
+            if password:
+                payload["password"] = password
+            payload = json.dumps(payload)
+            print('payload sigin',payload)
+            r = requests.post(rest_api_url, params={"key": my_firebase_api_key}, data=payload)
+            try:
+                data = r.json()
+                user_info = {
+                    'email': data['email'],
+                    'username': data.get('displayName')  # Retrieve username if available
+                }
+                return user_info
+            except:
+                st.warning(data)
+        except Exception as e:
+            st.warning(f'Signin failed: {e}')
+
+    def reset_password(email):
+        try:
+            rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode"
+            payload = {
+                "email": email,
+                "requestType": "PASSWORD_RESET"
+            }
+            payload = json.dumps(payload)
+            r = requests.post(rest_api_url, params={"key": my_firebase_api_key}, data=payload)
+            if r.status_code == 200:
+                return True, "Reset email Sent"
+            else:
+                # Handle error response
+                error_message = r.json().get('error', {}).get('message')
+                return False, error_message
+        except Exception as e:
+            return False, str(e)
+
+    # Example usage
+    # email = "example@example.com"
+           
+
+    def f(): 
+        try:
+            # user = auth.get_user_by_email(email)
+            # print(user.uid)
+            # st.session_state.username = user.uid
+            # st.session_state.useremail = user.email
+
+            userinfo = sign_in_with_email_and_password(st.session_state.email_input,st.session_state.password_input)
+            st.session_state.username = userinfo['username']
+            st.session_state.useremail = userinfo['email']
+
+            
+            global Usernm
+            Usernm=(userinfo['username'])
+            
+            st.session_state.signedout = True
+            st.session_state.signout = True    
+  
+            
+        except: 
+            st.warning('Login Failed')
+
+    def t():
+        st.session_state.signout = False
+        st.session_state.signedout = False   
+        st.session_state.username = ''
+
+
+    # def forget():
+    #     email = st.text_input('Email')
+    #     if st.button('Send Reset Link'):
+    #         print(email)
+    #         success, message = reset_password(email)
+    #         if success:
+    #             st.success("Password reset email sent successfully.")
+    #         else:
+    #             st.warning(f"Password reset failed: {message}") 
+        
+        
+    if "signedout"  not in st.session_state:
+        st.session_state["signedout"] = False
+    if 'signout' not in st.session_state:
+        st.session_state['signout'] = False    
+        
+
+        
+    
+    if  not st.session_state["signedout"]: # only show if the state is False, hence the button has never been clicked
+        choice = st.selectbox('Login/Signup',['Login','Sign up'])
+        email = st.text_input('Email Address')
+        password = st.text_input('Password',type='password')
+        st.session_state.email_input = email
+        st.session_state.password_input = password
+
+        
+
+        
+        if choice == 'Sign up':
+            username = st.text_input("다른 사람에게 표시될 닉네임을 입력해주세요")
+            verify_num = st.text_input('회원가입을 위한 인증코드를 입력해주세요')                
+            
+            if st.button('Create my account'):
+                if verify_num == '0503':
+                    # user = auth.create_user(email = email, password = password,uid=username)
+                    user = sign_up_with_email_and_password(email=email,password=password,username=username)
+                    
+                    st.success('회원가입 완료! 이제 로그인해주세요')
+                    st.markdown('Please Login using your email and password')
+                    st.balloons()
+
+        else:
+            # st.button('Login', on_click=f)          
+            st.button('Login', on_click=f)
+            # # if st.button('Forget'):
+            # forget()
+            # # st.button('Forget',on_click=forget)
+
+            
+            
+    if st.session_state.signout:
+                st.text('Name '+st.session_state.username)
+                st.text('Email id: '+st.session_state.useremail)
+                st.button('Sign out', on_click=t) 
+            
+                
+    
+
+                            
+    def ap():
+        st.write('Posts')
---
+++ trending.py
@@ -0,0 +1,192 @@
+from firebase_admin import firestore
+from firebase_admin import storage
+import pathlib
+import subprocess
+
+import ffmpeg
+import streamlit as st
+##### 기본 정보 입력 #####
+import streamlit as st
+# audiorecorder 패키지 추가
+from audiorecorder import audiorecorder
+# OpenAI 패키기 추가
+import openai
+# 파일 삭제를 위한 패키지 추가
+import os
+# 시간 정보를 위핸 패키지 추가
+from datetime import datetime
+# TTS 패키기 추가
+from gtts import gTTS
+# 음원파일 재생을 위한 패키지 추가
+import base64
+
+##### 기능 구현 함수 #####
+def STT(audio, user):
+    # 파일 저장
+    filename=user+'_'+datetime.now().strftime("%H:%M:%S")+'.mp3'
+    audio.export(filename, format="mp3")
+    # 음원 파일 열기
+    audio_file = open(filename, "rb")
+    #Whisper 모델을 활용해 텍스트 얻기
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+    # DB에 저장
+    bucket = storage.bucket()
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)
+
+    audio_file.close()
+    # 파일 삭제
+    # os.remove(filename)
+    return transcript["text"]
+
+def ask_gpt(prompt, model):
+    response = openai.ChatCompletion.create(model=model, messages=prompt)
+    system_message = response["choices"][0]["message"]
+    return system_message["content"]
+
+def TTS(response):
+    # gTTS 를 활용하여 음성 파일 생성
+    filename = "output.mp3"
+    tts = gTTS(text=response,lang="ko")
+    tts.save(filename)
+
+    # 음원 파일 자동 재성
+    with open(filename, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio autoplay="True">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(md,unsafe_allow_html=True,)
+    # 파일 삭제
+    os.remove(filename)
+
+##### 메인 함수 #####
+def app():
+    if 'db' not in st.session_state:
+        st.session_state.db = ''
+
+    db=firestore.client()
+    st.session_state.db=db
+    
+
+    try:
+        # session state 초기화
+        if "chat" not in st.session_state:
+            st.session_state["chat"] = []
+
+        if "messages" not in st.session_state:
+            st.session_state["messages"] = [{"role": "system", "content": "You are a thoughtful assistant. Respond to all input in 25 words and answer in korea"}]
+
+        if "check_reset" not in st.session_state:
+            st.session_state["check_reset"] = False
+
+        # 제목 
+        st.header("음성 챗봇 기본형")
+        # 구분선
+        st.markdown("---")
+
+        # 기본 설명
+        with st.expander("사용하는 법 (클릭하면 닫을 수 있습니다)", expanded=True):
+            st.write(
+            """     
+            - 먼저, 좌측 상단 사이드바를 열어 OpenAI API 키를 입력해 주세요.
+            - 다음으로 답변 생성에 사용될 GPT 모델을 설정해 주세요.
+            - 초기화 버튼을 누르면 이전 대화 내용이 리셋됩니다.
+            - 이제 사이드바를 닫고, 클릭하여 녹음하기 버튼을 눌러 챗봇에게 질문하세요.
+            - 질문이 끝나면 녹음중... 버튼을 눌러 녹음을 종료하세요
+            - 질문에 대한 답변이 음성으로 출력되고 채팅 내용이 오른쪽에 표시됩니다.
+            - 방금 내가 질문한 내용은 왼쪽 하단에서 들어볼 수 있습니다.
+            """
+            )
+
+            st.markdown("")
+
+        # 사이드바 생성
+        with st.sidebar:
+
+            # Open AI API 키 입력받기
+            openai.api_key = st.text_input(label="OPENAI API 키", placeholder="Enter Your API Key", value="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", type="password")
+
+            st.markdown("---")
+
+            # GPT 모델을 선택하기 위한 라디오 버튼 생성
+            model = st.radio(label="GPT 모델",options=["gpt-4", "gpt-3.5-turbo"])
+
+            st.markdown("---")
+
+            # 리셋 버튼 생성
+            if st.button(label="초기화"):
+                # 리셋 코드 
+                st.session_state["chat"] = []
+                st.session_state["messages"] = [{"role": "system", "content": "You are a thoughtful assistant. Respond to all input in 25 words and answer in korea"}]
+                st.session_state["check_reset"] = True
+                
+        # 기능 구현 공간
+        col1, col2 =  st.columns(2)
+        with col1:
+            # 왼쪽 영역 작성
+            st.subheader("질문하기")
+            # 음성 녹음 아이콘 추가
+            audio = audiorecorder("클릭하여 녹음하기", "녹음중...")
+            if (audio.duration_seconds > 0) and (st.session_state["check_reset"]==False):
+                # 음성 재생 
+                st.audio(audio.export().read())
+                # 음원 파일에서 텍스트 추출
+                question = STT(audio, st.session_state.username)
+
+                # 채팅을 시각화하기 위해 질문 내용 저장
+                now = datetime.now().strftime("%H:%M")
+                st.session_state["chat"] = st.session_state["chat"]+ [("user",now, question)]
+                # GPT 모델에 넣을 프롬프트를 위해 질문 내용 저장
+                st.session_state["messages"] = st.session_state["messages"]+ [{"role": "user", "content": question}]
+
+                # DB에 질문 내용 저장
+                info = db.collection('Chat').document(st.session_state.username).get()
+                if info.exists:
+                    info = info.to_dict()
+                    if 'Content' in info.keys():
+                        pos=db.collection('Chat').document(st.session_state.username)
+                        pos.update({u'Content': firestore.ArrayUnion([u'{}'.format(question)])})
+                    else:
+                        data={"Content":[question],'Username':st.session_state.username}
+                        db.collection('Chat').document(st.session_state.username).set(data)
+                else:
+                    data={"Content":[question],'Username':st.session_state.username}
+                    db.collection('Chat').document(st.session_state.username).set(data)
+
+        with col2:
+            # 오른쪽 영역 작성
+            st.subheader("질문/답변")
+            if  (audio.duration_seconds > 0)  and (st.session_state["check_reset"]==False):
+                #ChatGPT에게 답변 얻기
+                response = ask_gpt(st.session_state["messages"], model)
+
+                # GPT 모델에 넣을 프롬프트를 위해 답변 내용 저장
+                st.session_state["messages"] = st.session_state["messages"]+ [{"role": "system", "content": response}]
+
+                # 채팅 시각화를 위한 답변 내용 저장
+                now = datetime.now().strftime("%H:%M")
+                st.session_state["chat"] = st.session_state["chat"]+ [("bot",now, response)]
+
+                # 채팅 형식으로 시각화 하기
+                for sender, time, message in st.session_state["chat"]:
+                    if sender == "user":
+                        st.write(f'<div style="display:flex;align-items:center;"><div style="background-color:#007AFF;color:white;border-radius:12px;padding:8px 12px;margin-right:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', unsafe_allow_html=True)
+                        st.write("")
+                    else:
+                        st.write(f'<div style="display:flex;align-items:center;justify-content:flex-end;"><div style="background-color:lightgray;border-radius:12px;padding:8px 12px;margin-left:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', unsafe_allow_html=True)
+                        st.write("")
+                
+                # gTTS 를 활용하여 음성 파일 생성 및 재생
+                TTS(response)
+            else:
+                st.session_state["check_reset"] = False
+    except:
+        if st.session_state.username=='':
+            st.success('Please Login first')
+        else:
+            st.success('Check API Key')
